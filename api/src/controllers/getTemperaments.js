06558a1ddd87d8e7b1cb1c086fc0965847115479
Daniel Tello <daniel.qui.tello@gmail.com>
2023-10-02 13:20:49 -0500

    get temper in DB ok

--- api/src/controllers/getTemperaments.js
+++ api/src/controllers/getTemperaments.js
@@ -6,13 +6,31 @@ const apiKey = process.env.API_KEY
 
 const getTemperaments = async () => {
     const {data} = await axios.get(`${URL}?api_key=${apiKey}`)
-    console.log(data);
+    
     const temperaments = data.map((dog) => {
         return {
-            temperament: dog.temperament
+        temperament: dog.temperament
         }
     })
-    return temperaments
+
+    const singleTemper = temperaments.map((dog) => {
+        if(dog.temperament){
+            return {
+                temperament: dog.temperament.split(",").map((t) => 
+                    t.trim()
+                )
+            }
+        }
+        return dog
+    })
+
+    const dbTemperaments = await Temper.bulkCreate(
+        singleTemper.map((temperament) => ({
+          name: JSON.stringify(temperament),
+        }))
+      );
+   
+    return singleTemper
 }
 
 module.exports = {
--- api/src/handlers/getTemperamentsHandler.js
+++ api/src/handlers/getTemperamentsHandler.js
@@ -6,6 +6,7 @@ const getTemperamentsHandler = async (req, res) => {
         const temperaments = await getTemperaments()
         res.status(200).json(temperaments)
     } catch (error) {
+        console.log("Error fetching and saving:", error);
         res.status(500).json({error: error.message})
 
     }
