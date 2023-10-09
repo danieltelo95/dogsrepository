b798583db72bbe2cedaf02170c85d27fb52f614f
Daniel Tello <daniel.qui.tello@gmail.com>
2023-10-08 23:49:37 -0500

    dogsDetail actualizado

--- client/src/components/Card/Detail/Detail.jsx
+++ client/src/components/Card/Detail/Detail.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom'
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 
 const Detail = () => {
     const { id } = useParams()
